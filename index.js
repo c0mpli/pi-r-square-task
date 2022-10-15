5de28bb7e1d006a83a6d22d92366100c9b6fc96a
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2022-10-15 23:07:48 +0530

    upload_files done

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
 const express = require('express');
-var jwt = require('jsonwebtoken') ;
+var jwt = require('jsonwebtoken');
+const multer= require('multer');
+const path = require('path');
+const cookieparser = require("cookie-parser");
+
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
 const app = express();
+app.use(cookieparser());
+
+const storage = multer.diskStorage({
+    destination: './public/upload',
+    filename: (req,file,cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({
+    storage:storage
+})
 
 app.post('/create_new_storage', (req,res)=>{
     //auth user
     const username = {username:"jash"};
     //console.log(req.body.username) //ye kyu error dera hai?
     const token = jwt.sign(username,process.env.TOKEN_SECRET);
-    res.json({token:token});
+    return res
+    .cookie('token',token)
+    .json(
+        {
+            "status": "ok",
+            "message": "Storage Created Successfully",
+        }        
+    );
 })
 
 
-app.post('/upload_file',ensureToken,(req,res)=>{
+app.post('/upload_file',upload.single('profile'),ensureToken,(req,res)=>{
     jwt.verify(req.token,process.env.TOKEN_SECRET,function(err,data){
         if(err){
-            res.sendStatus(403);
+           return res.sendStatus(403);
         } else{
-            res.json({message:"Upload files."});
+            console.log(req.file)
         }
     })
 })
 
 function ensureToken(req,res,next){
-    const bearerHeader = req.headers["authorization"];
+    const bearerHeader = req.cookies.token;
     if(typeof bearerHeader !== 'undefined'){
-        const bearer = bearerHeader.split(" ");
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
+        req.token = bearerHeader;
         next();
     } else {
         res.sendStatus(403);
@@ -35,8 +57,6 @@ function ensureToken(req,res,next){
 }
 
 
-
-
-app.listen(3000,function(){
-    console.log("Listening on port 3000");
+app.listen(PORT,function(){
+    console.log(`Listening on port ${PORT}`);
 })
